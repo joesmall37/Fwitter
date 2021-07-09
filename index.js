32bdd500be8d4691934f52238f4af32d3ba36f29
Joe Small <small.joseph@outlook.com>
2021-07-08 20:17:28 -0400

    changes to server

--- index.js
+++ index.js
@@ -12,13 +12,22 @@ dotenv.config();
 app.use(express.json());
 app.use("/images", express.static(path.join(__dirname, "/images")));
 
-mongoose
-  .connect(process.env.MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify:true
-  })
+// mongoose
+//   .connect(process.env.MONGO_URL, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//     useCreateIndex: true,
+//     useFindAndModify:true
+//   })
+  mongoose.connect(
+     process.env.MONGODB_URI || 'mongodb://localhost/socialappwork',
+     {
+       useNewUrlParser: true,
+       useUnifiedTopology: true,
+       useCreateIndex: true,
+       useFindAndModify: false
+     }
+   )
   .then(console.log("Connected to MongoDB"))
   .catch((err) => console.log(err));
 
