efe22beb28ae4ff812adeef2c3d4733a8651958b
Joe Small <small.joseph@outlook.com>
2021-07-12 19:27:00 -0400

    comments section

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ const postRoute = require("./routes/posts");
 const multer = require("multer");
 const path = require("path");
 const session = require('express-session');
+const http = require ('http');
+const { Server } = require ('socket.io');
+
+
 dotenv.config();
 
 
@@ -75,6 +79,104 @@ if (process.env.NODE_ENV === "production") {
 }
 
 
-app.listen(PORT, () => {
-  console.log("Server is running at " + PORT);
+
+
+
+
+
+
+
+const httpServer = http.Server(app);
+const io = new Server(httpServer, { cors: { origin: '*' } });
+const users = [];
+
+io.on('connection', (socket) => {
+  console.log('connection', socket.id);
+  socket.on('disconnect', () => {
+    const user = users.find((x) => x.socketId === socket.id);
+    if (user) {
+      user.online = false;
+      console.log('Offline', user.name);
+      const admin = users.find((x) => x.isAdmin && x.online);
+      if (admin) {
+        io.to(admin.socketId).emit('updateUser', user);
+      }
+    }
+  });
+  socket.on('onLogin', (user) => {
+    const updatedUser = {
+      ...user,
+      online: true,
+      socketId: socket.id,
+      messages: [],
+    };
+    const existUser = users.find((x) => x._id === updatedUser._id);
+    if (existUser) {
+      existUser.socketId = socket.id;
+      existUser.online = true;
+    } else {
+      users.push(updatedUser);
+    }
+    console.log('Online', user.name);
+    const admin = users.find((x) => x.isAdmin && x.online);
+    if (admin) {
+      io.to(admin.socketId).emit('updateUser', updatedUser);
+    }
+    if (updatedUser.isAdmin) {
+      io.to(updatedUser.socketId).emit('listUsers', users);
+    }
+  });
+
+  socket.on('onUserSelected', (user) => {
+    const admin = users.find((x) => x.isAdmin && x.online);
+    if (admin) {
+      const existUser = users.find((x) => x._id === user._id);
+      io.to(admin.socketId).emit('selectUser', existUser);
+    }
+  });
+
+  socket.on('onMessage', (message) => {
+    if (message.isAdmin) {
+      const user = users.find((x) => x._id === message._id && x.online);
+      if (user) {
+        io.to(user.socketId).emit('message', message);
+        user.messages.push(message);
+      }
+    } else {
+      const admin = users.find((x) => x.isAdmin && x.online);
+      if (admin) {
+        io.to(admin.socketId).emit('message', message);
+        const user = users.find((x) => x._id === message._id && x.online);
+        user.messages.push(message);
+      } else {
+        io.to(socket.id).emit('message', {
+          name: 'Twitter',
+          body: 'Thank you for your message!',
+        });
+      }
+    }
+  });
 });
+
+httpServer.listen(PORT, () => {
+  console.log(`Serve at http://localhost:${PORT}`);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// app.listen(PORT, () => {
+//   console.log("Server is running at " + PORT);
+// });
