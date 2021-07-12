bfe33cbfb9da6e3e405eab96767546c809b96906
Joe Small <small.joseph@outlook.com>
2021-07-12 19:58:57 -0400

    socket

--- index.js
+++ index.js
@@ -78,14 +78,6 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-
-
-
-
-
-
-
-
 const httpServer = http.Server(app);
 const io = new Server(httpServer, { cors: { origin: '*' } });
 const users = [];
@@ -97,41 +89,6 @@ io.on('connection', (socket) => {
     if (user) {
       user.online = false;
       console.log('Offline', user.name);
-      const admin = users.find((x) => x.isAdmin && x.online);
-      if (admin) {
-        io.to(admin.socketId).emit('updateUser', user);
-      }
-    }
-  });
-  socket.on('onLogin', (user) => {
-    const updatedUser = {
-      ...user,
-      online: true,
-      socketId: socket.id,
-      messages: [],
-    };
-    const existUser = users.find((x) => x._id === updatedUser._id);
-    if (existUser) {
-      existUser.socketId = socket.id;
-      existUser.online = true;
-    } else {
-      users.push(updatedUser);
-    }
-    console.log('Online', user.name);
-    const admin = users.find((x) => x.isAdmin && x.online);
-    if (admin) {
-      io.to(admin.socketId).emit('updateUser', updatedUser);
-    }
-    if (updatedUser.isAdmin) {
-      io.to(updatedUser.socketId).emit('listUsers', users);
-    }
-  });
-
-  socket.on('onUserSelected', (user) => {
-    const admin = users.find((x) => x.isAdmin && x.online);
-    if (admin) {
-      const existUser = users.find((x) => x._id === user._id);
-      io.to(admin.socketId).emit('selectUser', existUser);
     }
   });
 
@@ -151,7 +108,7 @@ io.on('connection', (socket) => {
       } else {
         io.to(socket.id).emit('message', {
           name: 'Twitter',
-          body: 'Thank you for your message!',
+          body: 'Thank you for your comment!',
         });
       }
     }
@@ -159,12 +116,8 @@ io.on('connection', (socket) => {
 });
 
 httpServer.listen(PORT, () => {
-  console.log(`Serve at http://localhost:${PORT}`);
+  console.log(`Serve is listening at port ${PORT}`);
 });
-
-
-
-
 
 
 
