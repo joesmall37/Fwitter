34ff17b21582171dad16c528ab1a10bab013ed73
Joe Small <small.joseph@outlook.com>
2021-07-13 20:27:28 -0400

    changed .env

--- index.js
+++ index.js
@@ -65,5 +65,5 @@ if (process.env.NODE_ENV === "production") {
 }
 
 app.listen(PORT, () => {
-  console.log("Server is running at " + PORT);
+  console.log("Server is currently running at " + PORT);
 });
